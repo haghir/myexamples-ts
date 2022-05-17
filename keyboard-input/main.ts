e75fe65c4db1696daf48926a4566f98986b4da14
haghir <47360153+haghir@users.noreply.github.com>
2022-05-17 23:30:00 +0900

    KeyUP event was added.

--- keyboard-input/main.ts
+++ keyboard-input/main.ts
@@ -9,14 +9,20 @@ function querySelector(selector: string): HTMLElement {
 window.onload = () => {
     const pressed: HTMLElement = querySelector("#pressed");
     const repeating: HTMLElement = querySelector("#repeating");
+    const keyup: HTMLElement = querySelector("#keyup");
 
     window.addEventListener("keydown", (e) => {
         if (!e.repeat) {
-            console.log(`${e.key} was pressed`);
+            console.log(`${e.key} was pressed.`);
             pressed.innerHTML = `${e.key}`;
         } else {
-            console.log(`${e.key} is repeating`);
+            console.log(`${e.key} is repeating.`);
             repeating.innerHTML = `${e.key}`;
         }
+    });
+
+    window.addEventListener("keyup", (e) => {
+        console.log(`${e.key} was released.`);
+        keyup.innerHTML = `${e.key}`;
     });
 };
